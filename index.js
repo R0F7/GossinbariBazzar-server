b4e222f8583ea4e13fd5d2e199b9fe0acff55318
R0F7 <wwwrafikhan075@gmail.com>
2024-11-20 11:56:02 +0600

    add jwt

--- index.js
+++ index.js
@@ -2,24 +2,30 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 7777;
 
-//middleware
 const corsOptions = {
   origin: ["http://localhost:5173", "http://localhost:5174"],
-  Credential: true,
+  credentials: true,
   optionSuccessStatus: 200,
 };
 
+//cookieOption
+const cookieOption = {
+  httpOnly: true,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  secure: process.env.NODE_ENV === "production",
+};
+
+//middleware
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wezoknx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,26 +34,47 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    
-    const db = client.db('GossainbariBazzer');
+    const db = client.db("GossainbariBazzer");
 
+    //auth related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+
+      res
+        // .cookie("token", token, {
+        //   httpOnly: true,
+        //   secure: process.env.NODE_ENV === "production" ? true : false,
+        //   sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        // })
+        .cookie("token", token, cookieOption)
+        .send({ success: true });
+    });
+
+    app.get("/logout", async (req, res) => {
+      res
+        .clearCookie("token", { ...cookieOption, maxAge: 0 })
+        .send({ success: true });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
 run().catch(console.dir);
-
-
 
 app.get("/", (req, res) => {
   res.send("GossainbariBazzer server is running");
